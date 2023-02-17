95ca90f7c14fe2345c14abde73f3cb3f5f293cf1
Tiago Abudi <tiagoabudi96@gmail.com>
2023-02-17 09:50:35 -0300

    feat: implementaçao
    implementando o upload do CSV para popular a tabela

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Papa from "papaparse"
 
-function App() {
+const App = () => {
+
+  const [data, setData] = React.useState([]);
+  const [columnArray, setColumnArray] = React.useState([]);
+  const [valuesArray, setValuesArray] = React.useState([]);
+
+  const handleFile = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (result) {
+        const columnArray = [];
+        const valuesArray = [];
+
+        result.data.map((d) => {
+          columnArray.push(Object.keys(d));
+          valuesArray.push(Object.values(d));
+        });
+        setData(result.data);
+        setColumnArray(columnArray[0]);
+        setValuesArray(valuesArray);
+      }
+    })
+  }
+
+  const limparArquivo = () => {
+    setData([]);
+    setColumnArray([]);
+    setValuesArray([]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="file"
+        name="file"
+        accept=".csv"
+        onChange={handleFile}
+        style={{ display: "block", margin: "10px auto" }}
+      ></input>
+      <button onClick={limparArquivo} style={{ display: "block", margin: "10px auto" }}>Limpar Arquivo</button>
+      
+
+      <table style={{ borderCollapse: "collapse", border: "1px solid black", margin: "5px auto" }}>
+        <thead>
+          <tr>
+            {columnArray.map((col, i) => (
+              <th style={{ border: "1px solid black" }} key={i}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {valuesArray.map((v, i) => (
+            <tr key={i}>
+              {v.map((value, i) => (
+                <td style={{ border: "1px solid black" }} key={i}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
